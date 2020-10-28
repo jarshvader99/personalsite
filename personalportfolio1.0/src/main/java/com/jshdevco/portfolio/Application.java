7c4a278d6f9f6831ad7f4d08682396f6194e3751
Josh Harrison <jarshvader@gmail.com>
2020-10-28 15:04:13 -0500

    Add files via upload

--- personalportfolio1.0/src/main/java/com/jshdevco/portfolio/Application.java
+++ personalportfolio1.0/src/main/java/com/jshdevco/portfolio/Application.java
@@ -1,10 +1,24 @@
 package com.jshdevco.portfolio;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 @SpringBootApplication
 public class Application {
+	
+	@RequestMapping("/")
+	public String home(HttpServletRequest request, Model model) {
+        
+		String msg = "Hello World!";
+        model.addAttribute("msg", msg);
+        
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
