58416dbf0eb136dd928343e3d88d09e69438a790
Josh Harrison <jarshvader@gmail.com>
2020-10-28 22:52:51 -0500

    Add files via upload

--- personalportfolio1.0/src/main/java/com/jshdevco/portfolio/home/HomeController.java
+++ personalportfolio1.0/src/main/java/com/jshdevco/portfolio/home/HomeController.java
@@ -1,23 +1,69 @@
 package com.jshdevco.portfolio.home;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jshdevco.portfolio.contact.ContactForm;
 
 @Controller
 public class HomeController {
+	
+	@Value("${movie.apiKey}")
+    private String apiKey;
+	
 	@RequestMapping("/")
-		public String home(HttpServletRequest request, Model model, @ModelAttribute ContactForm contactForm) {
-	        
-			String msg = "Hello World!";
-	        model.addAttribute("msg", msg);
-	        model.addAttribute("contactForm", contactForm);
-	        
-			return "index";
+	public String home(HttpServletRequest request, Model model, @ModelAttribute ContactForm contactForm) {
+        
+		String msg = "Hello World!";
+        model.addAttribute("msg", msg);
+        model.addAttribute("contactForm", contactForm);
+        
+		return "index";
+	}
+	
+	@RequestMapping("/getPopular")
+	public String getPopular(@RequestParam(name="page") String page, HttpServletRequest request, Model model) {
+        
+        final String uri = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey + "&language=en-US&page=" + page;
+	    RestTemplate restTemplate = new RestTemplate();
+	    ObjectMapper mapper = new ObjectMapper();
+	    String result = restTemplate.getForObject(uri, String.class);
+   	    // convert JSON array to List
+	    try 
+	    {
+			Popular popular = mapper.readValue(result, Popular.class);
+			model.addAttribute("popular", popular);
+			List<Result> results = new ArrayList<>();
+			for(Result detailInResult : popular.getResults())
+			{
+				results.add(detailInResult);
+				model.addAttribute("results", results);
+			}
+		} 
+	    catch (JsonMappingException e) 
+	    {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	    catch (JsonProcessingException e) 
+	    {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+        
+		return "popularMovies";
+	}
 }
--- personalportfolio1.0/src/main/resources/static/js/scripts.js
+++ personalportfolio1.0/src/main/resources/static/js/scripts.js
@@ -3,4 +3,60 @@ $(document).ready(function(){
 	$(".close").click("on", function(){
 		window.location = "/";
 	});
+	
+
+	function likeMovie(resultId)
+	{
+		console.log('hiee');
+		console.log(resultId);
+		$.ajax({
+		    url: "/likeMovie?movieId=" + resultId,
+		    type: "GET",
+		    dataType : "json",
+		    success: function(response){
+		    	console.log(response);
+		           if(response)
+		       	   {
+		       	   	console.log('updated');
+		       	   }
+		           else
+		       	   {
+		       	   	console.log('failed');
+		       	   }
+		    },
+			error: function(){
+				$('#like-movie-thumb-' + resultId).addClass("likeMovie");
+			}
+		});
+	}
+
+	function dislikeMovie(resultId)
+	{
+		console.log('dis');
+		console.log(resultId);
+		$.ajax({
+		    url: "/dislikeMovie?movieId=" + resultId,
+		    type: "GET",
+		    dataType : "json",
+		    success: function(response){
+		    	console.log(response);
+		           if(response)
+		       	   {
+		       	   	console.log('updated');
+		       	   }
+		           else
+		       	   {
+		       	   	console.log('failed');
+		       	   }
+		    },
+			error: function(){
+				$('#like-movie-thumb-' + resultId).removeClass("likeMovie");
+			}
+		});
+	}
+
+
+
+
+	
 });
