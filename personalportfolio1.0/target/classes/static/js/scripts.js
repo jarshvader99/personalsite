13eb7a1d6eb33d965a4e9ba2b561c968e5ec24d0
Josh Harrison <jarshvader@gmail.com>
2020-10-30 23:41:17 -0500

    Add files via upload
    added search bar for movies list

--- personalportfolio1.0/target/classes/static/js/scripts.js
+++ personalportfolio1.0/target/classes/static/js/scripts.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
 		window.location = "/";
 	});
 	
+	function myFunction() {
+	    var input, filter, ul, li, a, i, txtValue;
+	    input = document.getElementById("search-bar");
+	    filter = input.value.toUpperCase();
+	    ul = document.getElementById("myUL");
+	    li = ul.getElementsByTagName("li");
+	    for (i = 0; i < li.length; i++) {
+	    	a = li[i].getElementsByTagName("span")[0];
+	        txtValue = a.textContent || a.innerText;
+	        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+	            li[i].style.display = "";
+	        } else {
+	            li[i].style.display = "none";
+	        }
+	    }
+	}
 
 	function likeMovie(resultId)
 	{
